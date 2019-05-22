be5147f2c36b869c6dc3c190cda063382cf7bd68
Karel Suta <ksuta@redhat.com>
2019-05-22 19:39:23 +0200

    Retrieve token from OpenShift
    Also run httpd on port 8080

--- sources/start.sh
+++ sources/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 sed -i 's@<openshift-url-placeholder>@'$ROUTE_URL'@g' /usr/local/apache2/htdocs/index.html
+
+TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+sed -i 's@<authentication-token-placeholder>@'$TOKEN'@g' /usr/local/apache2/htdocs/index.html
+
+sed -i 's@Listen 80@Listen 8080@g' /usr/local/apache2/conf/httpd.conf
+
 httpd-foreground
